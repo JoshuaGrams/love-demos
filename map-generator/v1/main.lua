72c140b3e76f6ede80a799dfdf284b1f0a82ef6c
Josh Grams <josh@qualdan.com>
2017-02-02 05:12:43 -0500

    * map-generator/v1: draw rects from a table.

---
+++ map-generator/v1/main.lua
@@ -0,0 +1,26 @@
+function love.load()
+	tSz = 16
+
+	map = {
+		['10,10'] = { 10, 10 },
+		['11,12'] = { 11, 12 },
+		['12,12'] = { 12, 12 }
+	}
+
+	love.graphics.setBackgroundColor(36, 33, 18)
+	love.graphics.setColor(30, 140, 22)
+end
+
+function drawMap(map)
+	for k,v in pairs(map) do
+		drawFloor(v[1], v[2])
+	end
+end
+
+function drawFloor(x, y)
+	love.graphics.rectangle("fill", x*tSz, y*tSz, tSz, tSz)
+end
+
+function love.draw()
+	drawMap(map)
+end
